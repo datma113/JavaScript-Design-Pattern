55d4db5ab4eb3f9c5d77b62fe8769b13073dc63f
datma <datma113112111@gmail.com>
2021-07-12 13:55:43 +0700

    example 2: observer pattern...

--- observer.js
+++ observer.js
@@ -29,10 +29,62 @@ const Observer1 = function() {
 const Observer2 = function() {
      console.log(`subject has changed: notified observer 2`)
 }
-sub.subscribe(Observer1)
-sub.subscribe(Observer2)
-sub.notifyAllObserver()
+
+
+// sub.subscribe(Observer1)
+// sub.subscribe(Observer2)
+// sub.notifyAllObserver()
 
 /**
  * code example 2
  */
+
+function Account(firstName, lastName) {
+     this.observers = []
+     this.fullname = `${firstName} ${lastName}`
+}
+
+Account.prototype = {
+     subscribe: function(observer) {
+          this.observers.push(observer)
+     },
+     unsubscribe: function(observerToUnsub) {
+          this.observers = this.observers.filter(observer => observerToUnsub !== observer)
+     },
+     notifyToObserver: function(observer) {
+          const index = this.observers.indexOf(observer)
+          if(index) this.observers[index].call()
+     },
+     notifyAllObservers: function() {
+          this.observers.forEach(observer => {
+               observer.call()
+          })
+     }
+}
+
+const mainAccount = new Account('admin', 'admin')
+
+const EmailAccount = function() {
+     const fullName = mainAccount.fullname 
+     console.log(`email notify: ${fullName}`)    
+}
+const FacebookAccount = function() {
+     const fullName = mainAccount.fullname
+     console.log(`fb notify: ${fullName}`)    
+}
+const InstagramAccount = function() {
+     const fullName = mainAccount.fullname
+     console.log(`insta notify: ${fullName}`)    
+}
+
+mainAccount.subscribe(EmailAccount)
+mainAccount.subscribe(FacebookAccount)
+mainAccount.subscribe(InstagramAccount)
+
+mainAccount.notifyAllObservers()
+
+mainAccount.notifyToObserver(InstagramAccount)
+
+mainAccount.unsubscribe(InstagramAccount)
+
+mainAccount.notifyAllObservers()
